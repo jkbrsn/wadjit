a9647679cb079d1c0300aafb31d31f93d1dd436d
Jakob Ersson <11717405+jakobilobi@users.noreply.github.com>
2025-09-22 12:15:14 +0200

    style: move HTTP DNS options into task_http

--- task_http.go
+++ task_http.go
@@ -133,6 +133,20 @@ func (e *HTTPEndpoint) Validate() error {
 	return e.dnsPolicy.Validate()
 }
 
+// WithDNSPolicy attaches a DNS policy to the HTTP endpoint.
+func WithDNSPolicy(policy DNSPolicy) HTTPEndpointOption {
+	return func(ep *HTTPEndpoint) {
+		ep.dnsPolicy = policy
+	}
+}
+
+// WithDNSDecisionHook registers a callback invoked for each DNS decision.
+func WithDNSDecisionHook(cb DNSDecisionCallback) HTTPEndpointOption {
+	return func(ep *HTTPEndpoint) {
+		ep.dnsDecisionHook = cb
+	}
+}
+
 // WithHeader configures the HTTPEndpoint to use the provided header.
 func WithHeader(h http.Header) HTTPEndpointOption {
 	return func(ep *HTTPEndpoint) { ep.Header = h }
